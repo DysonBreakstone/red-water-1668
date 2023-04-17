5828b2906831c99e2b96d8b6e29439171647b95a
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-04-17 15:52:06 -0600

    chef show page shows most popular ingredients

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,8 @@ class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
   has_many :ingredients, through: :dishes
+
+  def top_three 
+    ingredients.select("count(ingredients.*) as total_count, ingredients.id, ingredients.name").group("ingredients.id").order(total_count: :desc).limit(3)
+  end
 end
--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -12,5 +12,16 @@ RSpec.describe "Show page" do
       expect(page). to have_content(@chef_3.name)
       expect(page).to have_link("#{@chef_3.name}'s ingredients", href: chef_ingredients_path(@chef_3) )
     end
+
+    it "has the chef's 3 most popular ingredients" do
+      @dish_ingredient_25 = @dish_3.dish_ingredients.create!(ingredient_id: @ingredient_3.id)
+      @dish_ingredient_26 = @dish_3.dish_ingredients.create!(ingredient_id: @ingredient_4.id)
+      visit chef_path(@chef_1)
+      
+      expect(page).to have_content("most popular ingredients")
+      expect(page).to have_content("ingredient 1")
+      expect(page).to have_content("ingredient 3")
+      expect(page).to have_content("ingredient 4")
+    end
   end
 end
