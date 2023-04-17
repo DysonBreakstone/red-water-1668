3348c918bb436d895f92f8300c76706ee49ece7c
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-04-17 14:30:28 -0600

    User story 1

--- spec/features/chefs/dishes/show_spec.rb
+++ spec/features/chefs/dishes/show_spec.rb
@@ -9,19 +9,23 @@ RSpec.describe "show page", type: :feature do
     it "shows dish's name, ingredient list, calorie count, and chef" do
       visit chefs_dish_path(@dish_1)
 
-      expect(page).to have_content("chef: chef 1")
       expect(page).to have_content("dish 1")
+      expect(page).to have_content("chef: chef 1")
       expect(page).to have_content("description: 1 dish")
       expect(page).to have_content("calories: 100")
-      expect(page).to have_content("ingredients: ingredient 1, ingredient 2")
+      expect(page).to have_content("ingredients:")
+      expect(page).to have_content("ingredient 1")
+      expect(page).to have_content("ingredient 2")
 
-      visit chef_dish_path(@dish_5)
+      visit chefs_dish_path(@dish_5)
 
-      expect(page).to have_content("chef: chef 2")
       expect(page).to have_content("dish 5")
+      expect(page).to have_content("chef: chef 2")
       expect(page).to have_content("description: 5 dish")
       expect(page).to have_content("calories: 500")
-      expect(page).to have_content("ingredients: ingredient 9, ingredient 10")
+      expect(page).to have_content("ingredients:")
+      expect(page).to have_content("ingredient 1")
+      expect(page).to have_content("ingredient 6")
     end
   end
 end
